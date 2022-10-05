e8943c324a14d6f281f71795cd98a7188ec2b8fa
shokhjakh0n1h <112059340+shokhjakh0n1h@users.noreply.github.com>
2022-10-05 19:16:19 +0500

    Update JavaVariableNames.java

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -2,13 +2,21 @@ package lang.print.gaps.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int 2morrow = 0;
+        int morrow = 0;
         int $money = 10;
-        int %hundred = 100;
-        int !back = 1000;
+        int hundred = 100;
+        int back = 1000;
         int _underscore = 10000;
-        int #sharp = 100000;
-        int *star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
+        int sharp = 100000;
+        int star = 1000000;
+        int badexampleofvarname = 1000000;
+        System.out.println(morrow);
+        System.out.println($money );
+        System.out.println(hundred);
+        System.out.println(back);
+        System.out.println(_underscore);
+        System.out.println(sharp);
+        System.out.println(star);
+        System.out.println(badexampleofvarname);
     }
 }
